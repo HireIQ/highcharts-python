236c1e4c1e95a0666634f2a53c962d1b2ff2c58d
Ben Olive <ben.olive@hireiqinc.com>
2011-10-17 14:38:04 -0400

    Prevent race condition in renderChart
    loading to the same element multiple times now have deterministic
    results. The last call is the one whose response is rendered, regardless
    of response order.

--- javascript/highcharts-python.js
+++ javascript/highcharts-python.js
@@ -5,7 +5,7 @@
  * For example, to load a chart generated at the url /charts/sample with
  * a custom title and render it to the div with id 'banana':
  * 
- *     $('#banana').renderChart('/charts/toomah', {title: {text: 'A Better Title'}})
+ *     $('#banana').renderChart('/charts/sample', {title: {text: 'A Better Title'}})
  *
  * Options that you override are exactly those given in the highcharts documentation at:
  *
@@ -14,16 +14,22 @@
 
 (function($){
     $.fn.renderChart = function(url, options) {
+        var batch = $.fn.renderChart.nextBatch;
+        $.fn.renderChart.nextBatch += 1;
         var objects = this;
+        objects.data('renderChart_batch', batch);
         options = options || {};
         $.getJSON(url, {}, function(config) {
             config = $.extend(true, config, options);
             config.chart = config.chart || {};
             objects.each(function() {
-                config.chart.renderTo = this;
-                new Highcharts.Chart(config); 
+                if ($(this).data('renderChart_batch') == batch) {
+                    config.chart.renderTo = this;
+                    new Highcharts.Chart(config);
+                }
             });
         });
         return this;
     };
+    $.fn.renderChart.nextBatch = 0;
 })(jQuery);
